73076e466ec3cfe6d524cdbfa7e63b348ea2b40d
kun18911 <trunghieu99u@gmail.com>
2024-06-24 22:30:14 +0700

    update code file index

--- js/Map/khaiBáoMap.js
+++ js/Map/khaiBáoMap.js
@@ -248,7 +248,7 @@ var  aString = StringToArray(giải_Ẩn_Ý(parts[6]))
             value_Mã= transformInBrackets(value_Mã, map);
         } // chuyển đổi sang mã mới
 
-
+        var textbox = document.getElementById('myTextbox');
         textbox.value = value_Mã
         textbox.select(); document.execCommand('copy'); // Copy vào clipboard
         document.querySelector(".mã_Coppy_Lưu_Game").classList.remove("nonee")
